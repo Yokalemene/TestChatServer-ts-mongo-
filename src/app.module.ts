5a322855debc14bdaa2507eaba86dcbbff6f757f
Yokalemene <yokalemene@internet.ru>
2024-07-21 04:40:47 +0300

    some changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -7,7 +8,17 @@ import { ChatModule } from './modules/chat-module';
 import { MessageModule } from './modules/message-module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/chat-db'),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
   UserModule,
   ChatModule,
   MessageModule,],
